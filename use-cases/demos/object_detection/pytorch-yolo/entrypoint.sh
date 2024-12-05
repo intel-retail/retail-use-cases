837d5e89148944d964e4090577cb2619bcb84f5b
Neethu Elizabeth Simon <neethu.elizabeth.simon@intel.com>
2024-12-05 15:20:39 -0700

    feat: intel optimized pieplines using pytoch

--- use-cases/demos/object_detection/pytorch-yolo/entrypoint.sh
+++ use-cases/demos/object_detection/pytorch-yolo/entrypoint.sh
@@ -9,7 +9,23 @@
 cid=$(date +%Y%m%d%H%M%S%N)
 echo "cid: $cid"
 
-PROFILE_NAME="demo_pytorch_object_detection"
+echo "USE_ULTRALYTICS: $USE_ULTRALYTICS"
+echo "INTEL_OPTIMIZED: $INTEL_OPTIMIZED"
 
-python3 object_detection/pytorch-yolo/yolov5_object_detection.py \
+if [ $USE_ULTRALYTICS -eq 1 ]; 
+then
+ if [ $INTEL_OPTIMIZED -eq 1 ]; 
+ then 
+ PROFILE_NAME="demo_intel_ultralytics_pytorch_object_detection"
+ python3 object_detection/pytorch-yolo/yolov5_ultralytics_object_detection.py \
+-m "$MQTT_HOSTNAME" -p "$MQTT_PORT" -t "$MQTT_TOPIC" -i "$INPUT_SRC" -in | tee >/tmp/results/r"$cid"_"$PROFILE_NAME".jsonl >(stdbuf -oL sed -n -e 's/^.*FPS: //p' | stdbuf -oL cut -d , -f 1 > /tmp/results/pipeline"$cid"_"$PROFILE_NAME".log)
+ else
+ PROFILE_NAME="demo_ultralytics_pytorch_object_detection"
+ python3 object_detection/pytorch-yolo/yolov5_ultralytics_object_detection.py \
 -m "$MQTT_HOSTNAME" -p "$MQTT_PORT" -t "$MQTT_TOPIC" -i "$INPUT_SRC" | tee >/tmp/results/r"$cid"_"$PROFILE_NAME".jsonl >(stdbuf -oL sed -n -e 's/^.*FPS: //p' | stdbuf -oL cut -d , -f 1 > /tmp/results/pipeline"$cid"_"$PROFILE_NAME".log)
+ fi
+else
+ PROFILE_NAME="demo_pytorch_object_detection"
+ python3 object_detection/pytorch-yolo/yolov5_object_detection.py \
+-m "$MQTT_HOSTNAME" -p "$MQTT_PORT" -t "$MQTT_TOPIC" -i "$INPUT_SRC" | tee >/tmp/results/r"$cid"_"$PROFILE_NAME".jsonl >(stdbuf -oL sed -n -e 's/^.*FPS: //p' | stdbuf -oL cut -d , -f 1 > /tmp/results/pipeline"$cid"_"$PROFILE_NAME".log)
+fi
